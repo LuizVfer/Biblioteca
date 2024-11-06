45ca62cbc82bbca35d36f83bacada8462cd00ee7
LuizVfer <fernandovieirasantos10@gmail.com>
2024-11-05 22:19:17 -0400

    update 5/11

--- src/main/java/biblioteca/modelo/Livro.java
+++ src/main/java/biblioteca/modelo/Livro.java
@@ -2,4 +2,51 @@ package main.java.biblioteca.modelo;
 
 public class Livro {
     
+    private int id;
+    private String titulo;
+    private String autor;
+    private String ISBN;
+    private int anoPublicacao;
+    private Boolean disponibilidade;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getISBN() {
+        return ISBN;
+    }
+    public void setISBN(String iSBN) {
+        ISBN = iSBN;
+    }
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+    public Boolean getDisponibilidade() {
+        return disponibilidade;
+    }
+    public void setDisponibilidade(Boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
+    }
+
+
+
+
 }
